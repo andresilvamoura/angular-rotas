8ed4e7cac8144a5c567d7c3ac1a8a2b982b546c2
Andre Moura <andremoura500@gmail.com>
2022-08-22 19:13:58 -0300

    feat: Adicionado rotas filhas no sub-modulo de produtos

---
+++ src/app/demos/arquitetura-componentes/editar-produtos/editar-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-produtos',
+  templateUrl: './editar-produtos.component.html',
+  styles: []
+})
+export class EditarProdutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/demos/arquitetura-componentes/produto.route.ts
+++ src/app/demos/arquitetura-componentes/produto.route.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditarProdutosComponent } from './editar-produtos/editar-produtos.component';
 import { ProdutoDashboardComponent } from './produto-dashboard/produto-dashboard.component';
+import { ProdutoAppComponent } from './produto.app.component';
 
 const produtoRouterConfig: Routes = [
-    { path: '', component: ProdutoDashboardComponent }
+    { path: '', component: ProdutoAppComponent, 
+    children: [
+        { path: '', component: ProdutoDashboardComponent },
+        { path: 'editar/:id', component: EditarProdutosComponent }
+    ] }
 ];
 
 @NgModule({
